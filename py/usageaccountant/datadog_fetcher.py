c7ed011a7342037a86fdcfb9ea61fb4d46147a3e
Alexander Tarasov <alex.tarasov@sentry.io>
2025-05-19 23:09:11 +0200

    black fix

--- py/usageaccountant/datadog_fetcher.py
+++ py/usageaccountant/datadog_fetcher.py
@@ -95,7 +95,9 @@ def parse_and_assert_kafka_config(kafka_config_file: TextIO) -> KafkaConfig:
     config_params = kafka_config["config_params"]
 
     if "sasl.password" in config_params:
-        config_params["sasl.password"] = os.path.expandvars(config_params["sasl.password"])
+        config_params["sasl.password"] = os.path.expandvars(
+            config_params["sasl.password"]
+        )
 
     assert isinstance(bootstrap_servers, Sequence)
     assert isinstance(config_params, Mapping)
